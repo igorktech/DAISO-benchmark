611e271b5d41451f7f3113b2eeb2526c7a6acbb5
Igor <igoranchikkuzmin@yandex.ru>
2024-05-17 09:50:01 +0200

    minor(main): delete debug files

--- test_load.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-dataset = load_dataset(
-  "igorktech/daiso",
-  revision="dev",
-  split="test",# tag name, or branch name, or commit hash
-  cache_dir = 'cache'
-)
-print(dataset[1])
